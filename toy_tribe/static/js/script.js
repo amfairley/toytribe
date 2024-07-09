e9b80de977c307d4ec85d7672ed9a4d9fccbf592
amfairley <adamfairley1990@gmail.com>
2024-07-09 13:02:22 +0100

    feature: initialise the materialize select element. Issue: #19

--- toy_tribe/static/js/script.js
+++ toy_tribe/static/js/script.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+    
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });
