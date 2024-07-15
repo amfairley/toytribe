91fc5aede4332edee5e37463390131bcf4c10d16
amfairley <adamfairley1990@gmail.com>
2024-07-15 15:37:21 +0100

    build: add comments

--- toy_tribe/__init__.py
+++ toy_tribe/__init__.py
@@ -4,19 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env
 
+# Create an instance of the Flask object
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
+# Define my database
 db = SQLAlchemy(app)
 
 from toy_tribe import routes
-
-# Import events and Review model to ensure event listeners work
-from .models import event
-from .models import Review
-
-# Register event listeners
-event.listen(Review, 'after_insert', Review.after_insert)
-event.listen(Review, 'after_update', Review.after_update)
-event.listen(Review, 'after_delete', Review.after_delete)
