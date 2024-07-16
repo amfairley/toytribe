74c9d2b34fca4d23e6e4a4d63d76ba92969176bf
amfairley <adamfairley1990@gmail.com>
2024-07-16 11:00:09 +0100

    deployment: update db links for heroku deployment

--- toy_tribe/__init__.py
+++ toy_tribe/__init__.py
@@ -7,7 +7,15 @@ if os.path.exists("env.py"):
 # Create an instance of the Flask object
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+# Select the database based on development status
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 # Define my database
 db = SQLAlchemy(app)
