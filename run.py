dc2dfac8997b6d63f22a57dea32dd76bfed840ca
amfairley <adamfairley1990@gmail.com>
2025-04-26 10:37:16 +0100

    UPDATE: fix deployment bugs coming from changing postgres server

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
-from toy_tribe import app
+from toy_tribe import create_app
 
+app = create_app()
 
-if __name__ ==  "__main__":
+if __name__ == "__main__":
     app.run(
-        host= os.environ.get("IP"),
-        port= int(os.environ.get("PORT")),
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
     )
--- toy_tribe/__init__.py
+++ toy_tribe/__init__.py
@@ -4,20 +4,33 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env
 
-# Create an instance of the Flask object
-app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+# Create db object without app
+db = SQLAlchemy()
 
-# Select the database based on development status
-if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-else:
-    uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
+
+# Create app function
+def create_app():
+    # Create an instance of the Flask object
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+    # Select the database based on development status
+    if os.environ.get("DEVELOPMENT") == "True":
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    else:
+        uri = os.environ.get("DATABASE_URL")
+        if uri.startswith("postgres://"):
+            uri = uri.replace("postgres://", "postgresql://", 1)
+        # This is the database connection showing Flask where to connect
         app.config["SQLALCHEMY_DATABASE_URI"] = uri
+        # Enforces ssl connection for heroku
+        app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+            "connect_args": {"sslmode": "require"}
+        }
 
-# Define my database
-db = SQLAlchemy(app)
+    # Define my database
+    db.init_app(app)
 
-from toy_tribe import routes
+    from toy_tribe import routes
+
+    return app
